e1a7887ca41607fef125fd1c0747c573da0efb36
Dawny Wu <dawny@dawny-Lenovo-G450.(none)>
2012-02-17 23:16:30 +0800

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'babe42e030d9cfd0690a847d7a2c43ab02b36cf32f5e04187fe99a0499b9c9d20e9a03dc05283ebc0f86b57dab4191a8ebfaa64dafac701b1bd2e8d4ce55a728'
